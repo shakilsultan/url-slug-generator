01804673d3b1ef3e306f684fb05759951f392671
Shakil Sultan Ali <shakil.sultan@gmail.com>
2016-04-17 22:55:38 +0500

    add functionality to remove apostrophes too.

--- url-slug-generator.js
+++ url-slug-generator.js
@@ -1,7 +1,7 @@
 var myArray = [];
 $("#submit").on('click', function() {
 	if ( $('#box').val() || $('#box').text() ) {
-		myArray = $('#box').val().replace(/\.|,/g, '').split('\n');
+		myArray = $('#box').val().replace(/\.|,|'/g, '').split('\n');
 		if ( $('#output').length === 0) {
 			$('form').append('<textarea id="output" rows="20" cols="50"></textarea>');
 		}
