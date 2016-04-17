5bbc7af02774679b8d567cc55f38a5486319ce8f
Shakil Sultan Ali <shakil.sultan@gmail.com>
2016-04-17 18:05:54 +0500

    disable submit button after one submission. Reset button enables it back. Also, fixed textarea from generating on every submit button (separately but the above fix resolves it too).

--- url-slug-generator.js
+++ url-slug-generator.js
@@ -1,24 +1,27 @@
 var myArray = [];
-
 $("#submit").on('click', function() {
 	if ( $('#box').val() || $('#box').text() ) {
 		myArray = $('#box').val().replace(/\.|,/g, '').split('\n');
-		$('form').append('<textarea id="output" rows="20" cols="50"></textarea>');
+		if ( $('#output').length === 0) {
+			$('form').append('<textarea id="output" rows="20" cols="50"></textarea>');
+		}
 		for( var i = 0; i<myArray.length; i++ ) {
 			var newValue = myArray[i].toLowerCase().replace(/ /g, "-");
 				if ( i === myArray.length - 1) {
 					$('#output').append(newValue);
+					$('#submit').attr('disabled', 'disabled');
 				} else {
 					$('#output').append(newValue + '\n');
 				}
 			}
 		} else {
- 			alert('This is a required field.');
+			alert('This is a required field.');
 		}
 	}
 );
 
 $("#reset_button").on('click', function(value) {
+	$('#submit').removeAttr('disabled');
 	$('#output').remove();
 }
 );
